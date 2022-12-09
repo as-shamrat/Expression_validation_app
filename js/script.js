84a5cab6a6a50f249aba2a5fc0f3d88911a558d2
as-shamrat <67526675+as-shamrat@users.noreply.github.com>
2022-12-09 23:28:42 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let emailSelector = document.querySelector(".email");
+let phoneSelector = document.querySelector(".phone");
+let postSelector = document.querySelector(".post");
+emailSelector.addEventListener("click", validate);
+phoneSelector.addEventListener("click", validate);
+postSelector.addEventListener("click", validate);
