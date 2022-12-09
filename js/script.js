f4fc44470debeb1437f22921989f39d332292442
as-shamrat <67526675+as-shamrat@users.noreply.github.com>
2022-12-09 23:29:55 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ let postSelector = document.querySelector(".post");
 emailSelector.addEventListener("click", validate);
 phoneSelector.addEventListener("click", validate);
 postSelector.addEventListener("click", validate);
+function validate(e) {
+  let userInput;
+  // console.log(e.target);
+  if (e.target.classList.contains("email")) {
+    let regexEmail =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    userInput = prompt("Please enter an email: ");
+    if (regexEmail.test(userInput)) {
+      renderMessage("Email is validated", "success");
+    } else {
+      renderMessage("Email is not validated", "error");
+    }
+  } else if (e.target.classList.contains("phone")) {
+    let regexPhone = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    userInput = prompt("Please enter a phone: ");
+    if (regexPhone.test(userInput)) {
+      renderMessage("Phone is validated", "success");
+    } else {
+      renderMessage("Phone is not validated", "error");
+    }
+  } else if (e.target.classList.contains("post")) {
+    let regexPost = /^\d{4}$/;
+    userInput = prompt("Please enter a post code: ");
+    if (regexPost.test(userInput)) {
+      renderMessage("Post code is validated", "success");
+    } else {
+      renderMessage("Post code is not validated", "error");
+    }
+  }
+}
